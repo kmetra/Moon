7ea9e6e441cfa5ab911b94ef186fa369fadc4e3b
GlebFedorovich <gleb.fedorovich239@mail.ru>
2018-05-18 19:59:17 +0300

    Corrections
    Add some simple unit tests
    Correct main, add general parameters

--- fourth_stage.py
+++ fourth_stage.py
@@ -1,4 +1,6 @@
 from math import cos,sin,log,radians,sqrt
+import unittest
+
 
 R_Moon = 1738000 # радиус Луны
 g_Moon = 1.62
@@ -7,8 +9,6 @@ M = 2355
 m = 2355
 fuel_exp = 5.1  #кг/с
 fuel_speed = 3050 #м/с
-x = 0
-y = R_Moon
 Vx = 0
 Vy = 0
 def blast_off( time, angle):
@@ -24,7 +24,10 @@ def blast_off( time, angle):
     high = sqrt(x ** 2 + y ** 2) - R_Moon
 
 
-def Blast():
+def Blast(x_start,y_start):
+    global x,y
+    x = x_start
+    y = y_start
     for i in range( 1, 62):
         blast_off( 1, 90 - i)
     blast_off( 135, 28)
@@ -33,3 +36,13 @@ def Blast():
     Vres = sqrt( Vx** 2 + Vy** 2)
     Hres = sqrt(x ** 2 + y ** 2) - R_Moon
     return(Vres, Hres)
+
+
+class TestBlastMethods(unittest.TestCase):
+
+    def testAssertTrue(self):
+        V_current, H_current= Blast(0, R_Moon)
+        self.assertTrue((1590 < V_current < 1650) and (49000 < H_current < 51000))
+
+if __name__ == '__main__':
+    unittest.main()
--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 import fourth_stage as four
-V_aft_bl,H_aft_Blast=four.Blast()
+
+#necessary constants
+R_Moon = 1738000
+
+
+x_start = 0
+y_start = R_Moon
+
+
+V_aft_bl , H_aft_bl = four.Blast(x_start, y_start)
+#print(V_aft_bl, H_aft_bl)
