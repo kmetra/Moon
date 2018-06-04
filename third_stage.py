b87c20245d117c24a391fa3fceb280a03e698c49
daniro-13ftf <rdr.300799@gmail.com>
2018-06-04 06:08:39 +0300

    initial_changes
    function defining the speed after getting the additional impuls to change the orbital path

---
+++ third_stage.py
@@ -0,0 +1,52 @@
+from math import sqrt
+
+G = 6.67e-11
+M = 7.36e22
+R = 1738000
+
+x = 0
+y = 1788000
+v_x = -1656.9
+v_y = 0
+int_period = 0.01
+
+def a_x_and_a_y (x, y, grav_const = G, mass_moon = M):
+    a_x = - (grav_const * mass_moon * x) / ((x**2 + y**2)**1.5)
+    a_y = - (grav_const * mass_moon * y) / ((x**2 + y**2)**1.5)
+    return (a_x, a_y)
+
+def speed_after_impuls (x, y, v_x, v_y, t = int_period):
+    height_finish = 1750000
+    min_speed = v_x + 11  #больше этой скорости брать смысла нет, так как минимальный радиус орбиты 50 км, учтено, чтобы после импульса ЛМ не врезался в поверхность луны
+    x_probe = x
+    y_probe = y
+    v_y_probe = v_y
+    while height_finish > 1741000:
+        min_speed += 0.5
+        x = x_probe
+        y = y_probe
+        v_x = min_speed
+        v_y = v_y_probe
+        while x <= 0:
+            a_x, a_y = a_x_and_a_y(x, y)
+            x = x + v_x * t + a_x * (t ** 2) / 2
+            y = y + v_y * t + a_y * (t ** 2) / 2
+            v_x = v_x + a_x * t
+            v_y = v_y + a_y * t
+        height_finish = abs(y)
+        print (min_speed, height_finish)
+    return (min_speed)
+
+print (speed_after_impuls(x, y, v_x, v_y))
+
+
+print (x, y, v_x, v_y)
+
+'''t = int_period
+while x <= 0:
+    a_x, a_y = a_x_and_a_y(x, y)
+    x = x + v_x * t + a_x * (t ** 2) / 2
+    y = y + v_y * t + a_y * (t ** 2) / 2
+    v_x = v_x + a_x * t
+    v_y = v_y + a_y * t
+print (x, y, v_x, v_y)'''
